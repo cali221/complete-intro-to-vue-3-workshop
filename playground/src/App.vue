59a905983be44c0e6c288fbf20ef0614f26ee7eb
Theresia Calista <theresiacalista57@gmail.com>
2025-01-20 00:21:13 +0700

    fixed

--- playground/src/App.vue
+++ playground/src/App.vue
@@ -2,9 +2,6 @@
 export default {
   data() {
     return {
-      count: 10,
-      counterTitle: "Counter Standard",
-      incrementAmount: 8,
       message: "Hello it works",
       listOfNumbers: [
         {
@@ -35,45 +32,10 @@ export default {
       ],
     };
   },
-  computed: {
-    displayTitle() {
-      if (this.count > 20) {
-        return "Counter Standard - Very Long";
-      } else {
-        return "Counter Standard";
-      }
-    },
-    optimizedIncrementAmount() {
-      return this.displayTitle.length * this.incrementAmount;
-    },
-  },
-  methods: {
-    incrementCount(newAmount, event) {
-      console.log(newAmount);
-      console.log(event);
-      this.count += this.optimizedIncrementAmount;
-    },
-  },
-  watch: {
-    count(newValue) {
-      if (newValue > 20) {
-        this.counterTitle += " Very Long";
-      }
-    },
-  },
 };
 </script>
 
 <template>
-  <h1>{{ displayTitle }}</h1>
-  <p :data-increment-by="incrementAmount">{{ count }}</p>
-  <button @click="incrementCount">Increment Count</button>
-  <h1>{{ incrementAmount }}</h1>
-  <p>{{ optimizedIncrementAmount }}</p>
-  <div>
-    <label for="incrementAmount">Increment by:</label>
-    <input type="text" v-model="incrementAmount" />
-  </div>
   <hr />
   <p v-if="message.length % 2 === 0">Even: {{ message.toUpperCase() }}</p>
   <p v-else>Odd: {{ message }}</p>
