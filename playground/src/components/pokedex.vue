d59912b5551538c9e482f6fddbb76e8df43e7a63
Theresia Calista <theresiacalista57@gmail.com>
2025-04-01 16:38:46 +0700

    setup function playground

---
+++ playground/src/components/pokedex.vue
@@ -0,0 +1,49 @@
+<script>
+// https://pokeapi.co/api/v2/pokemon?limit=151
+
+import {computed, ref, reactive} from "vue";
+
+export default{
+  async setup(){
+    const regionName = ref("Kanto");
+
+    const state = reactive({
+      elementType: 'lightning'
+    })
+
+    const elementTypeAllCaps = computed(() => {
+      return state.elementType.toUpperCase();
+    })
+
+    const pokedex = await fetch('https://pokeapi.co/api/v2/pokemon?limit=151').then((response) => response.json());
+
+    return{
+      regionName,
+      elementTypeAllCaps,
+      pokedex
+    };
+  },
+  computed: {
+    regionNameLowerCase(){
+      return this.regionName.toLowerCase()
+    }
+  },
+  methods: {
+    changeRegionName(){
+      this.regionName = 'Hoenn'
+    }
+  },
+  created(){
+    console.log(this.regionName);
+    console.log(this.pokedex);
+  },
+}
+</script>
+
+<template>
+    <h2>{{ regionName }}</h2>
+    <h3>{{ elementTypeAllCaps }}</h3>
+    <h3>{{ regionNameLowerCase }}</h3>
+    <button @click="changeRegionName">Change Region Name</button>
+    <pre>{{ pokedex }}</pre>
+</template>
