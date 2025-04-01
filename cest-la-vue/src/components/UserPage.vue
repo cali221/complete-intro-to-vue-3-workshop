737d5426e7d6e10bee2f2c7a98d8e1b7eaf2c024
Theresia Calista <theresiacalista57@gmail.com>
2025-04-02 00:08:05 +0700

    finished exercise 12

--- cest-la-vue/src/components/UserPage.vue
+++ cest-la-vue/src/components/UserPage.vue
@@ -1,7 +1,7 @@
 <script setup>
 import UserCard from "./UserCard.vue";
-import { defineProps, reactive } from "vue";
-
+import { defineProps } from "vue";
+import { userList } from '../composables/useUserStore'
 defineProps({
   title: {
     type: String,
@@ -11,10 +11,6 @@ defineProps({
 
 defineEmits(["update-user-list"]);
 
-const state = reactive({
-  userList: [],
-});
-
 async function fetchUsers() {
   const response = await fetch(
     "https://jsonplaceholder.typicode.com/users"
@@ -23,7 +19,7 @@ async function fetchUsers() {
   return response;
 }
 
-state.userList = await fetchUsers();
+userList.value = await fetchUsers();
 </script>
 
 <template>
@@ -31,7 +27,7 @@ state.userList = await fetchUsers();
     <h1>{{ title }}</h1>
     <ul>
       <UserCard
-        v-for="user in state.userList"
+        v-for="user in userList"
         :user="user"
         :key="`user-${user.id}`"
       />
---
+++ cest-la-vue/src/composables/useUserStore.js
@@ -0,0 +1,3 @@
+import {reactive, ref} from 'vue'
+
+export const userList = ref([]);
