aa580bfb0aa043346a9374f9568390b80be37cba
Theresia Calista <theresiacalista57@gmail.com>
2025-04-03 15:02:32 +0700

    finished exercise 16

--- cest-la-vue/src/main.js
+++ cest-la-vue/src/main.js
@@ -1,7 +1,7 @@
 import { createApp } from "vue";
 import { createRouter, createWebHashHistory } from "vue-router";
 import HomePage from "@/views/HomePage.vue";
-
+import { createPinia } from 'pinia';
 import App from "./App.vue";
 
 const app = createApp(App);
@@ -27,5 +27,8 @@ const router = createRouter({
   ],
 });
 
+const pinia = createPinia()
+
 app.use(router);
+app.use(pinia);
 app.mount("#app");
